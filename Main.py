806edffd043bc2d09894099e7db8ce9b78bfec36
Muhammad Elsaadany <177386510+MuhammadElsaadany@users.noreply.github.com>
2026-04-20 18:05:24 +0200

    Add nested parsing and anomaly detection
    Import ast and refactor fetch_and_parse (rename db_table->db_create_table) and improve rate-limit/log messages and exception wording. Add fetch_nested to parse stringified nested fields (using ast.literal_eval) and populate gst_kp_readings from geomagnetic_storms.allKpIndex. Introduce a generalized check_anomalies function that supports single or composite primary keys and marks alerted rows. Re-enable API fetch calls for solar_flares, geomagnetic_storms and coronal_mass_ejections and add anomaly checks for solar_flares and gst_kp_readings.

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import requests, os, sqlite3
+import requests, os, sqlite3, ast
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,17 +8,17 @@ if not api_key:
     raise ValueError("Couldn't fetch NASA_API_KEY")
 
 
-def fetch_and_parse(db_name, db_table, url, db_insert, db_keys):
+def fetch_and_parse(db_name, db_create_table, url, db_insert, db_keys):
     connection = sqlite3.connect(str(db_name))
     cursor = connection.cursor()
-    cursor.execute(db_table)
+    cursor.execute(db_create_table)
 
     try:
         response = requests.get(str(url) + api_key)
         
-        ratelimit = response.headers.get("X-Ratelimit-Limit", "Failed to load rate limit")
-        ratelimit_remaining = response.headers.get("X-Ratelimit-Remaining", "Failed to load remaining rate limit")
-        print("Remaining rate limit: " + str(ratelimit_remaining) + " of " + str(ratelimit))
+        ratelimit_maximum = response.headers.get("X-Ratelimit-Limit", "Failed to load maximum rate limit!")
+        ratelimit_remaining = response.headers.get("X-Ratelimit-Remaining", "Failed to load remaining rate limit!")
+        print("Remaining rate limit: " + str(ratelimit_remaining) + " of " + str(ratelimit_maximum))
             
         if response.status_code == 200:
             response_data = response.json()
@@ -34,176 +34,225 @@ def fetch_and_parse(db_name, db_table, url, db_insert, db_keys):
             raise Exception("Error: Not found! | status code: 404")
         
         elif response.status_code == 503:
-            raise Exception("Error: Service Unavailable. | status code: 503")
+            raise Exception("Error: Service is not available! | status code: 503")
         
         else:
-            raise Exception("one of the other 95316 status codes that I have no idea exists popped up! | status code: " + str(response.status_code))
+            raise Exception("Error: One of the other 95316 status codes that I have no idea exists popped up! | status code: " + str(response.status_code))
         
     except requests.exceptions.ConnectionError:
         raise Exception ("Connection failed!")
 
 
-# fetch_and_parse("maindata.db",
-                
-#     """CREATE TABLE IF NOT EXISTS solar_flares (    
-#     flrID TEXT PRIMARY KEY,
-#     catalog TEXT NOT NULL,
-#     beginTime TEXT NOT NULL,
-#     peakTime TEXT,
-#     endTime TEXT,
-#     classType TEXT NOT NULL,
-#     sourceLocation TEXT,
-#     note TEXT,
-#     submissionTime TEXT NOT NULL,
-#     link TEXT NOT NULL,
-#     activeRegionNum INTEGER,
-#     versionId INTEGER NOT NULL,
-#     instruments TEXT NOT NULL,
-#     linkedEvents TEXT,
-#     sentNotifications TEXT,
-#     alerted INTEGER DEFAULT 0)""",
 
-#     "https://api.nasa.gov/DONKI/FLR?api_key=",
+def fetch_nested(db_name, parent_table_name, db_create_table, db_insert,  stringified_key, foreign_key):
+    connection = sqlite3.connect(db_name)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute(db_create_table)
 
-#     """INSERT OR IGNORE INTO solar_flares (
-#     flrID,
-#     catalog,
-#     beginTime,
-#     peakTime,
-#     endTime,
-#     classType,
-#     sourceLocation,
-#     note,
-#     submissionTime,
-#     link,
-#     activeRegionNum,
-#     versionId,
-#     instruments,
-#     linkedEvents,
-#     sentNotifications) 
-#     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+    nested = cursor.execute("SELECT " + str(stringified_key) + ", " + str(foreign_key) + " FROM " + str(parent_table_name)).fetchall()
 
-#     ["flrID",
-#      "catalog",
-#      "beginTime",
-#      "peakTime",
-#      "endTime",
-#      "classType",
-#      "sourceLocation",
-#      "note",
-#      "submissionTime",
-#      "link",
-#      "activeRegionNum",
-#      "versionId",
-#      "instruments",
-#      "linkedEvents",
-#      "sentNotifications"])
+    if nested:
+        for row in nested:
+            if row[str(stringified_key)] is not None:
+                un_nested = ast.literal_eval(row[str(stringified_key)])
+                for reading in un_nested:
+                    cursor.execute(db_insert, (row[str(foreign_key)], reading["observedTime"], reading["kpIndex"], reading["source"]))
+        connection.commit()
 
 
 
-# fetch_and_parse("maindata.db",
-                
-#     """CREATE TABLE IF NOT EXISTS geomagnetic_storms (    
-#     gstID TEXT PRIMARY KEY,
-#     startTime TEXT NOT NULL,
-#     allKpIndex TEXT NOT NULL,
-#     link TEXT NOT NULL,
-#     linkedEvents TEXT,
-#     submissionTime TEXT NOT NULL,
-#     versionId INTEGER NOT NULL,
-#     sentNotifications TEXT,
-#     alerted INTEGER DEFAULT 0)""",
-
-#     "https://api.nasa.gov/DONKI/GST?api_key=",
-
-#     """INSERT OR IGNORE INTO geomagnetic_storms (
-#     gstID,
-#     startTime,
-#     allKpIndex,
-#     link,
-#     linkedEvents,
-#     submissionTime,
-#     versionId,
-#     sentNotifications) 
-#     VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
-
-#     ["gstID",
-#     "startTime",
-#     "allKpIndex",
-#     "link",
-#     "linkedEvents",
-#     "submissionTime",
-#     "versionId",
-#     "sentNotifications"])
-
-
-
-# fetch_and_parse("maindata.db",
-                
-#     """CREATE TABLE IF NOT EXISTS coronal_mass_ejections (    
-#     activityID TEXT PRIMARY KEY,
-#     catalog TEXT NOT NULL,
-#     startTime TEXT NOT NULL,
-#     instruments TEXT NOT NULL,
-#     sourceLocation TEXT NOT NULL,
-#     activeRegionNum INTEGER,
-#     note TEXT,
-#     submissionTime TEXT NOT NULL,
-#     versionId INTEGER NOT NULL,
-#     link TEXT NOT NULL,
-#     cmeAnalyses TEXT NOT NULL,
-#     linkedEvents TEXT,
-#     sentNotifications TEXT,
-#     alerted INTEGER DEFAULT 0)""",
-
-#     "https://api.nasa.gov/DONKI/CME?api_key=",
-
-#     """INSERT OR IGNORE INTO coronal_mass_ejections (
-#     activityID,
-#     catalog,
-#     startTime,
-#     instruments,
-#     sourceLocation,
-#     activeRegionNum,
-#     note,
-#     submissionTime,
-#     versionId,
-#     link,
-#     cmeAnalyses,
-#     linkedEvents,
-#     sentNotifications) 
-#     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-
-#     ["activityID",
-#     "catalog",
-#     "startTime",
-#     "instruments",
-#     "sourceLocation",
-#     "activeRegionNum",
-#     "note",
-#     "submissionTime",
-#     "versionId",
-#     "link",
-#     "cmeAnalyses",
-#     "linkedEvents",
-#     "sentNotifications"])
-
-    def check_anomalies(db_name, table_name, execute_call, keys, primary_key):
-        connection = sqlite3.connect(db_name)
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        anomaly = cursor.execute(execute_call).fetchall()
-        if anomaly:
-            print("---------")
-            print("WARNING! SIGNIFICANT ANOMALIES DETECTED!")
-            print("---------")
-            for row in anomaly:
-                for key in keys:
-                    print(str(key) + ": " + str(row[key]))
+def check_anomalies(db_name, table_name, execute_call, keys, primary_key, primary_key2=None):
+    connection = sqlite3.connect(db_name)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    anomaly = cursor.execute(execute_call).fetchall()
+    if anomaly:
+        print("---------" + "\n" + "WARNING! SIGNIFICANT ANOMALIES DETECTED!" + "\n" + "---------")
+        for row in anomaly:
+            print("anomalyCategory: " + str(table_name))
+            for key in keys:
+                print(str(key) + ": " + str(row[key]))
+            if primary_key and not primary_key2:
                 cursor.execute("UPDATE " + str(table_name) + " SET alerted = 1 WHERE " + str(primary_key) + " = ?", (str(row[primary_key]), ))
-                connection.commit()
-                print("---------")
+            elif primary_key and primary_key2:
+                cursor.execute("UPDATE " + str(table_name) + " SET alerted = 1 WHERE " + str(primary_key) + " = ? AND " + str(primary_key2) + " = ?", (str(row[primary_key]), str(row[primary_key2])))
+            connection.commit()
+            print("---------")
 
 
 
-    check_anomalies("maindata.db", "solar_flares", "SELECT * FROM solar_flares WHERE (classType LIKE 'X%' OR classType LIKE 'M5%' OR classType LIKE 'M6%' OR classType LIKE 'M7%' OR classType LIKE 'M8%' OR classType LIKE 'M9%') AND alerted = 0", ["flrID", "beginTime", "classType", "sourceLocation", "activeRegionNum"], "flrID")
+fetch_and_parse("maindata.db",
+                
+    """CREATE TABLE IF NOT EXISTS solar_flares (    
+    flrID TEXT PRIMARY KEY,
+    catalog TEXT NOT NULL,
+    beginTime TEXT NOT NULL,
+    peakTime TEXT,
+    endTime TEXT,
+    classType TEXT NOT NULL,
+    sourceLocation TEXT,
+    note TEXT,
+    submissionTime TEXT NOT NULL,
+    link TEXT NOT NULL,
+    activeRegionNum INTEGER,
+    versionId INTEGER NOT NULL,
+    instruments TEXT NOT NULL,
+    linkedEvents TEXT,
+    sentNotifications TEXT,
+    alerted INTEGER DEFAULT 0)""",
+
+    "https://api.nasa.gov/DONKI/FLR?api_key=",
+
+    """INSERT OR IGNORE INTO solar_flares (
+    flrID,
+    catalog,
+    beginTime,
+    peakTime,
+    endTime,
+    classType,
+    sourceLocation,
+    note,
+    submissionTime,
+    link,
+    activeRegionNum,
+    versionId,
+    instruments,
+    linkedEvents,
+    sentNotifications) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+
+    ["flrID",
+     "catalog",
+     "beginTime",
+     "peakTime",
+     "endTime",
+     "classType",
+     "sourceLocation",
+     "note",
+     "submissionTime",
+     "link",
+     "activeRegionNum",
+     "versionId",
+     "instruments",
+     "linkedEvents",
+     "sentNotifications"])
+
+
+
+fetch_and_parse("maindata.db",
+                
+    """CREATE TABLE IF NOT EXISTS geomagnetic_storms (    
+    gstID TEXT PRIMARY KEY,
+    startTime TEXT NOT NULL,
+    allKpIndex TEXT NOT NULL,
+    link TEXT NOT NULL,
+    linkedEvents TEXT,
+    submissionTime TEXT NOT NULL,
+    versionId INTEGER NOT NULL,
+    sentNotifications TEXT,
+    alerted INTEGER DEFAULT 0)""",
+
+    "https://api.nasa.gov/DONKI/GST?api_key=",
+
+    """INSERT OR IGNORE INTO geomagnetic_storms (
+    gstID,
+    startTime,
+    allKpIndex,
+    link,
+    linkedEvents,
+    submissionTime,
+    versionId,
+    sentNotifications) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+
+    ["gstID",
+    "startTime",
+    "allKpIndex",
+    "link",
+    "linkedEvents",
+    "submissionTime",
+    "versionId",
+    "sentNotifications"])
+
+
+
+fetch_and_parse("maindata.db",
+                
+    """CREATE TABLE IF NOT EXISTS coronal_mass_ejections (    
+    activityID TEXT PRIMARY KEY,
+    catalog TEXT NOT NULL,
+    startTime TEXT NOT NULL,
+    instruments TEXT NOT NULL,
+    sourceLocation TEXT NOT NULL,
+    activeRegionNum INTEGER,
+    note TEXT,
+    submissionTime TEXT NOT NULL,
+    versionId INTEGER NOT NULL,
+    link TEXT NOT NULL,
+    cmeAnalyses TEXT NOT NULL,
+    linkedEvents TEXT,
+    sentNotifications TEXT,
+    alerted INTEGER DEFAULT 0)""",
+
+    "https://api.nasa.gov/DONKI/CME?api_key=",
+
+    """INSERT OR IGNORE INTO coronal_mass_ejections (
+    activityID,
+    catalog,
+    startTime,
+    instruments,
+    sourceLocation,
+    activeRegionNum,
+    note,
+    submissionTime,
+    versionId,
+    link,
+    cmeAnalyses,
+    linkedEvents,
+    sentNotifications) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+
+    ["activityID",
+    "catalog",
+    "startTime",
+    "instruments",
+    "sourceLocation",
+    "activeRegionNum",
+    "note",
+    "submissionTime",
+    "versionId",
+    "link",
+    "cmeAnalyses",
+    "linkedEvents",
+    "sentNotifications"])
+
+
+
+fetch_nested("maindata.db",
+             
+             "geomagnetic_storms",
+             
+            """CREATE TABLE IF NOT EXISTS gst_kp_readings (    
+            gstID TEXT NOT NULL,
+            observedTime TEXT NOT NULL,
+            kpIndex REAL NOT NULL,
+            source TEXT NOT NULL,
+            alerted INTEGER DEFAULT 0,
+            PRIMARY KEY (gstID, observedTime))""",
+            
+            """INSERT OR IGNORE INTO gst_kp_readings (    
+            gstID,
+            observedTime,
+            kpIndex,
+            source)
+            VALUES (?, ?, ?, ?)""",
+            
+            "allKpIndex",
+            
+            "gstID")
+
+
+
+check_anomalies("maindata.db", "solar_flares", "SELECT * FROM solar_flares WHERE (classType LIKE 'X%' OR classType LIKE 'M5%' OR classType LIKE 'M6%' OR classType LIKE 'M7%' OR classType LIKE 'M8%' OR classType LIKE 'M9%') AND alerted = 0", ["flrID", "beginTime", "classType", "sourceLocation", "activeRegionNum"], "flrID")
+check_anomalies("maindata.db", "gst_kp_readings", "SELECT * FROM gst_kp_readings WHERE kpIndex > 7 AND alerted = 0", ["gstID", "observedTime", "kpIndex", "source"], "gstID", "observedTime")
